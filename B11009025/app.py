fd1e46e37120a4088c4118e76cd537e18c909bb1
bruce20701 <131954867+bruce20701@users.noreply.github.com>
2023-10-19 15:19:08 +0800

    first commit

---
+++ B11009025/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import urllib.request as request
+import json
+
+src="https://cloud.hakka.gov.tw/Pub/Opendata/DTST20231000002.json"
+with request.urlopen(src) as response:
+    data = json.load(response)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('viewpoint.html', data = data)
+    
+if __name__ == '__main__' : 
+    app.run()
